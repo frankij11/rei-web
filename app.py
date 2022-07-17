56c1457102276455eb57530c43d2f7212bca66e4
frankij11 <kevinfjoy@gmail.com>
2022-07-16 22:33:28 -0400

    updated app to be able to run

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import panel as pn
 import pandas as pd
 import hvplot.pandas
+import holoviews as hv
 import numpy as np
+import ipyleaflet as leaflet
+
 from rei import sdat
 from rei import search_homes as s
 
 auc = pd.concat([s.auction_hw(), s.auction_ac()])
 redfin = s.redfin()
-redfin = redfin.assign(url = redfin["url_see_http://www.redfin.com/buy-a-home/comparative-market-analysis_for_info_on_pricing"] )
+redfin = redfin.assign(url = lambda x: x.filter(regex="url_*"))
 pn.config.sizing_mode = 'stretch_width'
 
 
@@ -19,11 +22,15 @@ def comps(address="1303 Alberta Dr"):
         comps = sdat.add_features(comps)
     except:
         pass
+
     app = pn.Card(
             pn.Column(
-                pn.Card(meta[['address','price', 'sqft', 'acre']], title="Meta Data"),
-                pn.Card(comps[['address','price','sqft','acre']], title="Comparable Data"),
-                pn.Card(comps.hvplot(x='sqft', y='price', kind='scatter', color="basement"))
+                pn.Card(meta[['address','price', 'sqft', 'acre','basement','style','type', 'url']], title="Meta Data"),
+                pn.Card(comps.query("sqft >@meta.sqft.max()*.8 and sqft <@meta.sqft.max()*1.2").price.describe(), title="ARV"),
+                pn.Card(comps.hvplot(x='sqft', y='price', kind='scatter', color="basement") *\
+                       hv.VLine(meta.sqft.max(), color='blue')),
+                pn.Card(comps['address','price', 'sqft', 'acre','basement','style','type', 'url']], title="Comparable Data")
+                
                 )
         )
     return app
@@ -31,7 +38,11 @@ def comps(address="1303 Alberta Dr"):
 app = pn.template.BootstrapTemplate(title="KJ's REI APP", header_background='blue',header_color="red")
 
 pg_comp_calc = pn.Column(pn.interact(comps, address="1303 Alberta Dr"))
+
+
+#map_auction = leaflet.Map().add_layer()
 pg_auc = pn.Column(
+                #pn.Card(pn.panel(map_auction)),
                 pn.Card(pn.widgets.DataFrame(auc), title="Auction Properties")
             )
 pg_redfin = pn.Column(
