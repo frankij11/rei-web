d78b9cb0a9399d4cf45f4fd1c8d48a2257268bd5
frankij11 <kevinfjoy@gmail.com>
2023-08-25 09:58:07 -0400

    use perspective instead of  dataframe

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from rei import search_homes as s
 pn.extension("perspective")
 
 auc = pd.concat([s.auction_hw(), s.auction_ac()])
+#auc_comps = sdat.Homes(auc.address)
 redfin = s.redfin()
 redfin = redfin.assign(url = lambda x: x.filter(regex="url_*"))
 pn.config.sizing_mode = 'stretch_width'
@@ -30,10 +31,12 @@ def comps(address="1303 Alberta Dr"):
     app = pn.Card(
             pn.Column(
                 pn.Card(meta[cols], title="Meta Data"),
-                pn.Card(comps.query("sqft >@meta.sqft.max()*.8 and sqft <@meta.sqft.max()*1.2").price.describe(), title="ARV"),
+                pn.Card(
+                    comps.query("sqft >@meta.sqft.max()*.8 and sqft <@meta.sqft.max()*1.2").price.describe(), title="ARV"
+                    ),
                 pn.Card(comps.hvplot(x='sqft', y='price', kind='scatter', color="basement") *\
                        hv.VLine(meta.sqft.max(), color='blue')),
-                pn.Card(comps[cols], title="Comparable Data")
+                pn.Card(pn.pane.Perspective(comps[cols]), title="Comparable Data")
                 
                 )
         )
@@ -47,7 +50,7 @@ pg_comp_calc = pn.Column(pn.interact(comps, address="1303 Alberta Dr"))
 #map_auction = leaflet.Map().add_layer()
 pg_auc = pn.Column(
                 #pn.Card(pn.panel(map_auction)),
-                pn.Card(pn.widgets.DataFrame(auc), title="Auction Properties")
+                pn.Card(pn.pane.Perspective(pn.widgets.DataFrame(auc)), title="Auction Properties")
             )
 pg_redfin = pn.Column(
                 pn.Card(pn.widgets.DataFrame(redfin[["address", "city","price", "square_feet", "url"]] ), title="Redfin Properties")
